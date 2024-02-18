8493bec42c3f443c094089f7b05a12d786793991
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-02-17 18:44:49 -0800

    Now each book is displayed in a "card" and each card contains an "X" button that allows the user to remove the book from the library. Clicking the button will remove the card div from the DOM and the book object from the array

--- library.js
+++ library.js
@@ -8,13 +8,36 @@ function Book(name, author){
 function addBookToLibrary(name, author){
     book = new Book(name, author)
     myLibrary.push(book)
+    
+    let cards=document.getElementById("cards");
+    let card=document.createElement("div");
+    card.id="card";
+    card.textContent=book.name+", " + book.author;
+    let closeButton = document.createElement("button");
+    closeButton.textContent="X";
+    
+    closeButton.addEventListener("click",()=>{
+        card.remove();
+        let index=myLibrary.indexOf(book)
+        myLibrary.splice(index,1);
+    });
+    card.append(closeButton);
+    cards.append(card);
 };
+
+
+function displayCards(){
+    
+     myLibrary.forEach((book)=>{
+        
+     });
+     console.log(myLibrary);
+    
+}
 
 let newBook = document.getElementById("newBook");
 newBook.addEventListener("click",()=>{
     let name=prompt("Please state the name of the book");
     let author=prompt("Please state the author of the book");
-    addBookToLibrary(name,author)
+    addBookToLibrary(name,author);
 });
-
-console.log(myLibrary);
